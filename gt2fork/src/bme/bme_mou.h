2ef2d1320717a447f80c1a736a17f6e65d0bd42f
Jan Wassermann <jan.wassermann@jansalleine.com>
2021-09-03 00:05:12 +0200

    getting rid of bme dir preparation for overhaul of legacy.c/.h

--- gt2fork/src/bme/bme_mou.h
+++
@@ -1,7 +0,0 @@
-// BME mouse functions header file
-
-void mou_init(void);
-void mou_uninit(void);
-void mou_getpos(unsigned *x, unsigned *y);
-void mou_getmove(int *dx, int *dy);
-unsigned mou_getbuttons(void);
