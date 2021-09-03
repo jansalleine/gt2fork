a9bfa78f3e3c15b7aad50411c686bf9a1d4f0017
Jan Wassermann <jan.wassermann@jansalleine.com>
2021-09-03 12:09:59 +0200

    remove old gt2 directories - move gt2fork one dir up

--- gt2stereo/src/bme/bme.c
+++
@@ -1,8 +0,0 @@
-//
-// BME (Blasphemous Multimedia Engine) main module
-//
-
-#include "bme_err.h"
-#include "bme_cfg.h"
-
-int bme_error = BME_OK;
--- gt2stereo/src/bme/bme_joy.h
+++
@@ -1,4 +0,0 @@
-// Joystick functions header file
-
-int joy_detect(unsigned id);
-unsigned joy_getstatus(unsigned id, int threshold);
